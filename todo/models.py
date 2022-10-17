67256ff1b237df79a0b3db6aa1fc00ed21e82846
Alberto Zarzo <stroncio198@gmail.com>
2022-10-17 12:02:15 +0200

    Add files via upload

---
+++ todo/models.py
@@ -0,0 +1,11 @@
+from email.policy import default
+from turtle import title
+from django.db import models
+
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+
+def __str__(self):
+    return self.title
